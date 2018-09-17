b11ca64dbdc93617e37378fb27ea18fe1b4dd1ad
jhon <jj.gonzalez10@uniandes.edu.co>
2018-09-16 22:06:31 -0500

    n

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb://admin1:admin1@ds147681.mlab.com:47681/proyecto2-database';
 
 mongoose.connect(URI)
+
   .then(db => console.log('Db is connected'))
   .catch(error => console.error(error));
 
